935fbed33958661d43102af2ba2ee1f4d0ad2985
MadNit <53405149+MadNit@users.noreply.github.com>
2022-09-02 19:03:59 +0530

    fixed mylist issue

--- list.py
+++ list.py
@@ -1,3 +1,3 @@
 # List ADT in Python
 my_list = list()   # Empty list
-print(f"The number of objects in this list are : {len(mylist)}")
+print(f"The number of objects in this list are : {len(my_list)}")
