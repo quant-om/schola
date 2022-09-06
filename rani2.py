ad2792fa178cfbbe92a0e85339e1e6d690e062fc
eswarirani <eswari.rani@outlook.com>
2022-09-06 17:32:58 +0530

    python operators

---
+++ rani2.py
@@ -0,0 +1,28 @@
+# python operators
+
+# 1 arithmatic operator
+#  +,-,*,/,//,%,**
+a = 5
+b = 6
+c = a + b
+
+
+# 2 assignment operator
+# =+, =-, =*, =/, =**, =//, =%
+
+
+# 3 unary operator
+# -unary (a = -a)
+
+
+# 4 relational operator
+# <, >, >=, <=, ==
+
+
+# 5 logical operator
+# or, and, not
+
+# 6 boolean operator
+# 7 bitwise operator
+# 8 membership operator
+# 9 identity operator
