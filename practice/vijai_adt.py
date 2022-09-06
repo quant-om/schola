b33e01f1908cec1d23b68e93e871b07298746f57
MadNit <53405149+MadNit@users.noreply.github.com>
2022-09-06 18:52:35 +0530

    adding adt

---
+++ practice/vijai_adt.py
@@ -0,0 +1,15 @@
+
+# Python Abstract Data Types
+# 1. list
+mylist = list()
+
+print(f"111- list is {mylist}")
+len_list = len(mylist)
+print(f"111- list len is {len_list}")
+
+mylist.append(1234)
+mylist.append("quantom")
+
+print(f"222- list is {mylist}")
+len_list = len(mylist)
+print(f"222- list len is {len_list}")
