1d1abf834d505171bc9094f2c887509da4d2f331
indumathikoda <110450708+indumathikoda@users.noreply.github.com>
2022-09-06 20:06:23 +0530

    New file of data types

---
+++ practice/indu_datatypes.py
@@ -0,0 +1,31 @@
+# Python basic data types
+# int datatype
+a = 6
+b = 8
+print(f"Integer = {a}")
+print(f"Type of number is {type(b)}")
+
+#float datatype
+a = 3.6
+print(f"the number is {a}")
+print(f"the type of the value is {type(a)}")
+
+
+# str datatype
+a = "happy"
+b = "-8"
+print(f"The string values are {a} and {b}")
+print(f"the type of values are {type(a)} and {type(b)}")
+
+#boolean datatype
+float = True
+string = False
+print(f"float is {float}")
+print(f"string is {string}")
+print(f"string type is {type(string)}")
+
+#None datatype
+data = None
+print(f"the Data {data} is {type(data)}")
+
+
