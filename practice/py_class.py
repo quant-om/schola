cac39733115f203d17d03e1f1814ff5f5a16805c
MadNit <53405149+MadNit@users.noreply.github.com>
2022-09-06 19:11:13 +0530

    class

---
+++ practice/py_class.py
@@ -0,0 +1,25 @@
+# Four pillars of Object Oriented Programming
+# 1. Abstraction
+# 2. Inheritance
+# 3. Encapsulation
+# 4. Polymorphism
+
+# Classes in Python
+class Animal:
+    def __init__(self):
+        animal_type = None
+        animal_color = None
+
+
+class Car:
+    def __init__(self):
+        car_type = "SUV"
+        car_model = "Maruti"
+        car_color = 'Blue'
+        car_manu = "Suzuki"
+        car_year = 2021
+        car_types = []
+
+
+mylist = list()
+myCar = Car()
