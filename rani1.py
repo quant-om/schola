7024736637499200f87e37b046a7b4250f884cc2
eswarirani <eswari.rani@outlook.com>
2022-09-06 17:10:34 +0530

    data types

---
+++ rani1.py
@@ -0,0 +1,28 @@
+# all python data types
+# 1.integer
+from operator import truediv
+
+
+a = 10
+print(f" value of a is {a}")
+print(f"type of a is {type(a)}")
+
+# 2. float
+a = 3.4
+print(f" the value of a is {a}")
+print(f"type of a is {type(a)}")
+
+# 3. string
+a = "123"
+print(f" value of a is {a}")
+print(f" type of a is {type(a)}")
+
+# 4. boolean
+a = True
+print(f" value of a is {a}")
+print(f" type of a is {type(a)}")
+
+# 5. None
+a = None
+print(f" value of a is {a}")
+print(f" type of a is{type(a)}")
