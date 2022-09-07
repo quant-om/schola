b6894efbd8d8b4f38251c6b73a405048c12116b8
eswarirani <eswari.rani@outlook.com>
2022-09-07 11:47:17 +0530

    rani new project

---
+++ practice/rani_data_types.py
@@ -0,0 +1,25 @@
+# all python data types
+# 1. integer
+a = 4
+print(f" value of a is {a}")
+print(f"type of a is {type(a)}")
+
+# 2. float
+b = 5.8
+print(f" value of b is {b}")
+print(f"type of b is {type(b)}")
+
+# 3. string
+c = 'stop'
+print(f"value of c is {c}")
+print(f" type of c is {type(c)}")
+
+# boolean
+d = False
+print(f"value of d is {d}")
+print(f"type of d is{type(d)}")
+
+# None
+e = None
+print(f"value of e is {e}")
+print(f" type of e is{type(e)}")
