42c67589f3fc25b5fcb1bc028d8ee962cd94fe7f
indumathikoda <110450708+indumathikoda@users.noreply.github.com>
2022-09-07 23:55:04 +0530

    file of control statements

---
+++ practice/indu_control_statements.py
@@ -0,0 +1,29 @@
+# Python control statements/structures
+# 1. if
+a = 9
+b = a % 2
+if b == 0:
+    print(f"{a} is even number")
+else:
+    print(f"{a} is odd number")
+
+# 2. for
+for i in range(3,12,2):
+    print(f"i is {i}")
+
+# 3. while
+# print numbers 1 to 8
+a = 1
+while a <= 8:
+    print(f"a = {a}")
+    a += 1
+
+# 4. assert / returns / break / continue
+
+
+
+
+    
+
+
+
